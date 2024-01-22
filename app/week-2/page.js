ff682bcac8cdc6bd1fd343d404f1a1d986c0e9ee
Kelvin <124754990+Akelvin51@users.noreply.github.com>
2024-01-22 10:12:35 -0700

    my web dev assignment 2

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info"
+export default function Page(){
+    return(
+    <div>
+        <h1>Shopping cart</h1>
+        <StudentInfo/> 
+    </div>
+      
+    );
+  }
