5425f8c0c40e2fa1de750c00d705cce5cd81a88d
Kelvin <124754990+Akelvin51@users.noreply.github.com>
2024-01-29 12:52:42 -0700

    add week 3 assignment
    add week 3 assignment

--- app/page.js
+++ app/page.js
@@ -2,7 +2,14 @@ import Link from "next/link";
 export default function Page(){
     return(
       <div>
-        <Link href="/week-2">CPRG 306: Web Development 2 - Assignments</Link>
+        <p>
+        <Link href="/week-2">CPRG 306: Web Development - Assignments 1</Link> 
+
+        </p>
+      
+        <p>
+        <Link href="/week-3">CPRG 306: Web Development  - Assignments 2</Link>
+      </p>
       </div>
     );
   }
